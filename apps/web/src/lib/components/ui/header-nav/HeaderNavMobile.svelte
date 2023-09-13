898dd04c7ca4b6280de4c32cc063b3fcb5ad8a46
Paul Brickles <pbrickles@gmail.com>
2023-09-13 17:03:58 +0100

    Improve burger menu

--- apps/web/src/lib/components/ui/header-nav/HeaderNavMobile.svelte
+++ apps/web/src/lib/components/ui/header-nav/HeaderNavMobile.svelte
@@ -2,14 +2,20 @@
 	import { Sheet, SheetContent, SheetHeader, SheetTrigger } from "$components/ui/sheet";
 	import type { Navigation } from "sanity-schema";
 	import NavItems from "./NavItems.svelte";
+	import BurgerIcon from "~icons/codicon/menu";
 
 	export let nav: Navigation;
 </script>
 
 <div class="md:hidden">
 	<Sheet>
-		<SheetTrigger>Burger Menu</SheetTrigger>
-		<SheetContent size="full">
+		<SheetTrigger>
+			<div class="flex flex-col items-center">
+				<BurgerIcon class="w-7 h-7" />
+				<span class="text-xs uppercase tracking-widest">Menu</span>
+			</div>
+		</SheetTrigger>
+		<SheetContent size="full" class="pt-0" closeClass="top-1">
 			<SheetHeader>
 				<NavItems {nav} />
 			</SheetHeader>
--- apps/web/src/lib/components/ui/sheet/SheetContent.svelte
+++ apps/web/src/lib/components/ui/sheet/SheetContent.svelte
@@ -1,30 +1,30 @@
 <script lang="ts">
 	import type { VariantProps } from "class-variance-authority";
-	import { X } from "lucide-svelte";
 	import { Dialog as SheetPrimitive } from "radix-svelte";
 	import { cn } from "$lib/utils";
 	import { sheetVariants } from ".";
 	import SheetOverlay from "./SheetOverlay.svelte";
 	import SheetPortal from "./SheetPortal.svelte";
+	import MenuToCloseIcon from "~icons/line-md/menu-to-close-transition";
 
 	let className: string | undefined | null = undefined;
 	export { className as class };
-	export let position: VariantProps<typeof sheetVariants>["position"] =
-		"right";
+	export let position: VariantProps<typeof sheetVariants>["position"] = "right";
 	export let size: VariantProps<typeof sheetVariants>["size"] = "default";
+	export let closeClass: string | undefined | null = undefined;
+
+	const closeIconClasses = cn(
+		"absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary",
+		closeClass
+	);
 </script>
 
 <SheetPortal {position}>
 	<SheetOverlay />
-	<SheetPrimitive.Content
-		class={cn(sheetVariants({ position, size }), className)}
-		{...$$restProps}
-	>
+	<SheetPrimitive.Content class={cn(sheetVariants({ position, size }), className)} {...$$restProps}>
 		<slot />
-		<SheetPrimitive.Close
-			class="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary"
-		>
-			<X class="h-4 w-4" />
+		<SheetPrimitive.Close class={closeIconClasses}>
+			<MenuToCloseIcon class="w-8 h-8" />
 			<span class="sr-only">Close</span>
 		</SheetPrimitive.Close>
 	</SheetPrimitive.Content>
